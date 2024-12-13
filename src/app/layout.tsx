5b9ca807a1a852e816d616803ad9dd47b52101e8
signalxu <fovzhao@126.com>
2024-12-13 19:45:00 +0100

    support opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,11 @@ import '../styles/globals.css'
 import '../styles/fonts.css'
 
 export const metadata: Metadata = {
-  title: 'RocketFi',
-  description: 'Rocket Pool DeFi Integration Explorer',
+  title: 'Rocket Pool DeFi Integration Explorer - RocketFi',
+  description: 'RocketFi is a community-driven, unofficial explorer for Rocket Pool DeFi opportunities. It helps rETH holders maximize their participation in DeFi to earn rewards and unlock the utility of rETH. Additionally, it supports node operators in leveraging DeFi to enhance their node operations.',
   openGraph: {
-    title: 'RocketFi',
-    description: 'Rocket Pool DeFi Integration Explorer',
+    title: 'Rocket Pool DeFi Integration Explorer - RocketFi',
+    description: 'RocketFi is a community-driven, unofficial explorer for Rocket Pool DeFi opportunities. It helps rETH holders maximize their participation in DeFi to earn rewards and unlock the utility of rETH. Additionally, it supports node operators in leveraging DeFi to enhance their node operations.',
     url: 'https://rocketfi.vercel.app/',
     siteName: 'RocketFi',
     locale: 'en_US',
